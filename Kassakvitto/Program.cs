8bb300b6dc8960c54b0e2d1f3fae23b2b336162f
Martin-Carlsson85 <martin@webfixit.se>
2021-09-09 14:26:53 +0200

    Added while-loops

--- Kassakvitto/Program.cs
+++ Kassakvitto/Program.cs
@@ -6,7 +6,65 @@ namespace Kassakvitto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double totalSum;
+            int sum;
+            int sumAfterRound;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Ange totalsumman för köpet:");
+                    totalSum = double.Parse(Console.ReadLine());
+                    break;
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("Felaktig totalsumma!");
+                }
+            }
+            sumAfterRound = (int)Math.Round(totalSum);
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Ange erhållet belopp från kund:");
+                    sum = int.Parse(Console.ReadLine());
+                    break;
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("Erhållet belopp är felaktigt");
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    if (totalSum > sum)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+
+                catch
+                {
+                    Console.WriteLine("Du har betalt för lite pengar");
+                    break;
+                }
+            }
+
+            Console.WriteLine(totalSum + sumAfterRound);
+           
         }
     }
 }
